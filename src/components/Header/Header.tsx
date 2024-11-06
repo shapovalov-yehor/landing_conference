1e0808bc0771f0d672101573d41e088228d21b70
Igor Gromadskiy <gromadskiyigor@gmail.com>
2024-11-06 12:14:55 +0200

    [Add] header section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,36 @@
 
 import Image from 'next/image';
 import styles from './Header.module.css';
-// import logo from '../../img/header/logo.webp';
-import Icon from '@/helpers/Icon';
+import logo from '../../img/header/logo.png';
+import { navItems } from '@/data/data';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <a className={styles.logo_wrap} href="/">
+          <Image
+            className={styles.logo}
+            src={logo}
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="logo"
+          />
+          <span className={styles.logo_text}>MUSTAGE</span>
+        </a>
+        <nav>
+          <ul className={styles.nav_list}>
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a className={styles.nav_link} href={item.href}>
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
---
+++ src/data/data.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+  { label: 'Кто мы такие', href: '#about-us' },
+  { label: 'Экосистема', href: '#ecosystem' },
+  { label: 'Наша ценность для сообщества', href: '#value' },
+  { label: 'Наши принципы', href: '#principles' },
+  { label: 'Достижения в цифрах', href: '#achievements' },
+  { label: 'Контакты', href: '#contacts' },
+];
